7733b9e619b4a20599b449d9f0792215ba0286f8
thatmich <47138011+thatmich@users.noreply.github.com>
2022-04-01 14:18:43 +0900

    react v18, fixed dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import * as ReactDOMClient from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// React 17
-// ReactDOM.render(
-//     <App />,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
 
-// React 18
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App tab="home" />)
